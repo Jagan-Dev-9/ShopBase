8155a3162c98ddc84140819aa7477f5a2a6dccc1
Jagan Sunkavalli <sunkavallisathvik@gmail.com>
2025-08-16 17:32:45 +0530

    cors update

--- backend/shopbase/settings.py
+++ backend/shopbase/settings.py
@@ -134,6 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'products')
 # CORS configuration
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "*",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3000",
